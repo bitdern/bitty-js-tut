6fd83b771c40bac015e9ec732291ed31b1aec0cb
aidan.newell <acnewell09@gmail.com>
2023-04-30 16:29:57 -0500

    added sending functionality

--- src/app.js
+++ src/app.js
@@ -2,4 +2,12 @@ const { testnet, mainnet } = require("bitcore-lib/lib/networks");
 const { createWallet, createHDWallet } = require("./bitty-wallet");
 const sendBitcoin = require("./send-bitty");
 
-console.log(createHDWallet(testnet));
+sendBitcoin("mkHS9ne12qx9pS9VojpwU5xtRd4T7X7ZUt", 0.02)
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+// console.log(createHDWallet(testnet));
